4a47955dae03fd639b40e520466ae80eaff5a2e3
Paul Kinlan <paul.kinlan@gmail.com>
2020-06-11 14:14:39 +0100

    Updating Time.. It should not use seconds or milliseconds.

--- build/client/client.js
+++ build/client/client.js
@@ -926,7 +926,7 @@ class Feed extends Log {
 
 function correctISOTime(date) {
   const tzoffset = (new Date()).getTimezoneOffset() * 60000;
-  return (new Date(date - tzoffset)).toISOString().slice(0, -1).replace(/Z$/, '');
+  return (new Date(date - tzoffset)).toISOString().replace(/:(\d+).(\d+)Z$/, '');
 }
 
 if ('navigator' in globalThis === false) globalThis.navigator = {
--- views/helpers/timezone.js
+++ views/helpers/timezone.js
@@ -1,4 +1,4 @@
 export default function(date) {
   const tzoffset = (new Date()).getTimezoneOffset() * 60000;
-  return (new Date(date - tzoffset)).toISOString().slice(0, -1).replace(/Z$/, '');
+  return (new Date(date - tzoffset)).toISOString().replace(/:(\d+).(\d+)Z$/, '');
 }
