a8b04f41a48757e128e24639f7df530045fc47f8
Paul Kinlan <paul.kinlan@gmail.com>
2020-06-14 14:12:23 +0100

    Making the SW lighter by filtering out assets that won't be used in cache.

--- tools/build-sw.js
+++ tools/build-sw.js
@@ -12,12 +12,16 @@ const args = arg({
   '-o': '--origin'
 });
 
+const findAny = (regexArray, value) => {
+  return regexArray.some(regex => regex.test(value));
+}
+
 let process = async (directory, origin, root, ignore) => {
   let urls = new Set();
   let allFiles = await readdir(`${directory}`, { withFileTypes: true, encoding: 'utf8' });
 
   let directories = allFiles.filter(file => file.isDirectory()).map(dir => path.join(directory, dir.name));
-  let files = allFiles.filter(file => file.isFile() && ignore.indexOf(path.join(directory.replace(root, '/'), file.name)) < 0 && file.name.startsWith('.') === false);
+  let files = allFiles.filter(file => file.isFile() && findAny(ignore, path.join(directory.replace(root, '/'), file.name)) == false && file.name.startsWith('.') === false);
 
   // Process the results
   files.forEach(file => urls.add(path.join(directory.replace(root, '/'), file.name)));
@@ -33,7 +37,7 @@ let process = async (directory, origin, root, ignore) => {
 const go = async (directories, origin, ignore) => {
   let urls = new Set();
   for (let directory of directories) {
-    const newUrls = await process(directory, origin, directory, ignore);
+    const newUrls = await process(directory, origin, directory, ignore.map(i => new RegExp(i)));
     urls = new Set([...urls, ...newUrls]);
   }
   console.log(JSON.stringify(Array.from(urls.values())));
