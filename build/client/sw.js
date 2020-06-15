04eea7b9852ee327d2436838b5a572553956af0d
Paul Kinlan <paul.kinlan@gmail.com>
2020-06-15 13:30:25 +0100

    Fixes #13 - adds favicon.

--- build/client/sw.js
+++ build/client/sw.js
@@ -5,6 +5,7 @@
     <script src="/client.js" type="module" defer></script>
     <link rel="stylesheet" href="/styles/main.css">
     <link rel="manifest" href="/manifest.json">
+    <link rel="shortcut icon" href="/images/icons/log/res/mipmap-hdpi/log.png">
     <meta name="viewport" content="width=device-width">
   </head>
   ${t}
--- views/partials/head.js
+++ views/partials/head.js
@@ -8,6 +8,7 @@ export const head = (data, body) => {
     <script src="/client.js" type="module" defer></script>
     <link rel="stylesheet" href="/styles/main.css">
     <link rel="manifest" href="/manifest.json">
+    <link rel="shortcut icon" href="/images/icons/log/res/mipmap-hdpi/log.png">
     <meta name="viewport" content="width=device-width">
   </head>
   ${body}
