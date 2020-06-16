681d3814b2384cfe9599d7c0aceae1c7eb895bf7
Paul Kinlan <paul.kinlan@gmail.com>
2020-06-16 22:55:16 +0100

    Adding default language.

--- views/partials/head.js
+++ views/partials/head.js
@@ -2,7 +2,7 @@ import html from "../lib/florawg.js";
 
 export const head = (data, body) => {
   return html`<!DOCTYPE html>
-    <html>
+    <html lang="en">
       <head>
         <title>Baby Logger</title>
         <script src="/client.js" type="module" defer></script>
