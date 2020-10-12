b5bcf6c7591837ba77eb377557e3a4a46621e521
Paul Kinlan <paul.kinlan@gmail.com>
2020-10-12 14:16:54 +0100

    Removing Firebase

--- build.sh
+++ build.sh
@@ -4,9 +4,3 @@ docker build --tag baby-logger:1.0 .
 # Need to ensure cloud build is enabled.
 gcloud builds submit --tag gcr.io/baby-logger-279914/web-server --project=baby-logger-279914
 gcloud run deploy --image gcr.io/baby-logger-279914/web-server --project=baby-logger-279914 --platform managed
-
-# Firebase
-git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
-cd cloud-builders-community/firebase
-gcloud builds submit . --project=baby-logger-279914
-rm -rf cloud-builders-community/
